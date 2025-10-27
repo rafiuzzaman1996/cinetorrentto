9cecc1bae1cdda329a2ec55ff54cc835b8e868cf
Nayeemr45 <nayeemr.45@gmail.com>
2025-10-27 23:59:40 +0600

    refactor

--- src/app/(admin)/admin-api/login/route.ts
+++ src/app/(admin)/admin-api/login/route.ts
@@ -29,7 +29,7 @@ export async function POST(req: Request) {
     response.cookies.set('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       path: '/',
       maxAge: 60 * 60, // 1 hour
     });
